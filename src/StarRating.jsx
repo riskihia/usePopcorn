795bd9269bf7e9b32c0a3a54e778dc8f7f730427
riskihia <riskirahmathia3@gmail.com>
2025-04-22 23:31:24 +0700

    feat: Add style to star, add maxRating props with default value

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -1,7 +1,29 @@
-export default function StarRating(){
+const containerStyle={
+    display: 'flex',
+    alignItems: 'center',
+    gap: '16px'
+};
+
+const starContainerStyle = {
+    display: 'flex',
+    gap: '4px'
+}
+
+const textStyle = {
+    lineHeight: '1',
+    margin :'0'
+}
+
+export default function StarRating({maxRating = 5}){
     return(
-        <div>
-            Hello
+        <div style={containerStyle}>
+            <div style={starContainerStyle}>
+                {Array.from( {length:5}, (_, i) => (
+                    <span>S {i + 1}</span>
+                )
+                )}
+            </div>
+            <p style={textStyle}>{maxRating}</p>
         </div>
     )
 }
