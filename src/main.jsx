0f11c8b99aac5ac281d0ea3471aff95f8dbab97e
riskihia <riskirahmathia3@gmail.com>
2025-04-23 13:57:41 +0700

    feat: Access rating value from parent

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode, useState } from "react";
+import { createRoot } from "react-dom/client";
 // import './app.css'
 // import App from './App.jsx'
-import StarRating from './StarRating'
-createRoot(document.getElementById('root')).render(
+import StarRating from "./StarRating";
+
+const textStyle = {
+  borderColor: "balck",
+  borderStyle: "solid",
+  // backgroundColor: "grey",
+};
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" onSetRating={setMovieRating} />
+      <p>This moveiw was rated {movieRating} yey </p>
+    </div>
+  );
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5}/>
-    <StarRating maxRating={10}/>
-    <StarRating/>
-  </StrictMode>,
-)
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "bad", "okay", "Good", "Amazing"]}
+    />
+    <StarRating maxRating={5} color="red" />
+    <StarRating size={24} color="pink" />
+    <StarRating style={textStyle} defaultRating={0} />
+    <Test />
+  </StrictMode>
+);
